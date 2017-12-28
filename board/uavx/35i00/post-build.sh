44df4b17d3ea31aff7f006b5431babdb75252852
Wi. System <wi@system.com>
2017-12-28 18:04:40 +0100

    Changes

---
+++ board/uavx/35i00/post-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp ${BINARIES_DIR}/imx6q_uavx_35i00.dtb ${BINARIES_DIR}/UAVX.dtb
---
+++ board/uavx/37i00/post-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp ${BINARIES_DIR}/imx6q_uavx_37i00.dtb ${BINARIES_DIR}/UAVX.dtb
